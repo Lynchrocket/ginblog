d9910efc096b603a5c9a1e768ed2a681b89bb5ea
Lynchrocket <77106522+Lynchrocket@users.noreply.github.com>
2022-07-02 22:45:08 +0800

    Delete ginblog directory

--- ginblog/models/Category.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type Category struct {
-	gorm.Model
-	Cid  uint8  `gorm:"type: primary_key; int; auto_increment" json:"cid"` // 分成第几类
-	Name string `gorm:"type: varchar(20); not null" json:"name"`
-}
